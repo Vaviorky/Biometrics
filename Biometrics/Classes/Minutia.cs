bc1e55c6baee2e61aca78ce94aceead127b2b327
Vaviorky <danieloss15@gmail.com>
2017-05-29 22:26:58 +0200

    need to find better solution

--- Biometrics/Classes/Minutia.cs
+++ Biometrics/Classes/Minutia.cs
@@ -13,7 +13,8 @@ namespace Biometrics.Classes
     {
         private static int _width, _height, _stride;
         private static byte[] _pixels;
-        private static int[,] _intPixels;
+        private static int[,] _intPixels, _tempIntPixels;
+
         private static List<int[]> _minutiaCandidates;
         public static BitmapSource MarkMinuties(BitmapSource source)
         {
@@ -31,9 +32,11 @@ namespace Biometrics.Classes
 
             //copy all data about pixels values into 1-dimentional array
             bitmap.CopyPixels(_pixels, _stride, 0);
-            int count3, count5;
-            bool square3WithCount3, square5WithCount3;
+
             InitIntImage();
+
+            _tempIntPixels = _intPixels;
+            float cnValue;
 
             for (int x = 0; x < _width; x++)
             {
@@ -45,10 +48,10 @@ namespace Biometrics.Classes
                         continue;
                     }
 
-                    count3 = 0;
-                    count5 = 0;
-                    square3WithCount3 = false;
-                    square5WithCount3 = false;
+                    int count3 = 0;
+                    int count5 = 0;
+                    bool square3WithCount3 = false;
+                    bool square5WithCount3 = false;
 
                     //square a = 5
                     for (int i = -1; i <= 1; i++)
@@ -64,49 +67,53 @@ namespace Biometrics.Classes
 
                             if (_intPixels[x + j, y + i] == 1)
                                 count3++;
-
+                            
                         }
                     }
 
                     if (count3 == 3)
+                        square3WithCount3 = true;
+                    else
+                        square5WithCount3 = false;
+
+                    //square a = 9
+                    for (int i = -4; i <= 4; i++)
                     {
-                        _intPixels[x, y] = 2;
+                        for (int j = -4; j <= 4; j++)
+                        {
+                            //check if i,j in border of sqare (a==5)
+                            if (i != -4 && i != 4 && j != -4 && j != 4)
+                                continue;
+
+                            if (x + j < 0 || x + j >= _width || y + i < 0 || y + i >= _height || (i == 0 && j == 0))
+                                continue;
+
+                            if (_intPixels[x + j, y + i] == 1)
+                                count5++;
+
+                        }
                     }
 
+                    if (count5 == 3)
+                        square5WithCount3 = true;
+                    else
+                    {
+                        square5WithCount3 = false;
+                    }
 
-
-                    /* //square a = 9
-                     for (int i = -4; i <= 4; i++)
-                     {
-                         for (int j = -4; j <= 4; j++)
-                         {
-                             //check if i,j in border of sqare (a==5)
-                             if (i != -4 && i != 4 && j != -4 && j != 4)
-                                 continue;
-
-                             if (x + j < 0 || x + j >= _width || y + i < 0 || y + i >= _height || (i == 0 && j == 0))
-                                 continue;
-
-                             if (_intPixels[x + j, y + i] == 1)
-                                 count5++;
-
-                             if (count5 == 3)
-                                 square5WithCount3 = true;
-
-                         }
-                     }
-
-                     //if both squares are 
-                     if (square3WithCount3 && square5WithCount3)
-                     {
-                         _minutiaCandidates.Add(new[]
-                         {
+                    //if both squares are 
+                    if (square3WithCount3 && square5WithCount3)
+                    {
+                        _minutiaCandidates.Add(new[]
+                        {
                              x,y
                          });
-                     }*/
+                    }
 
                 }
             }
+
+            MarkPotentialMinutiaes();
 
 
             RevertIntPixelsIntoPixelArray();
@@ -115,6 +122,57 @@ namespace Biometrics.Classes
             var rect = new Int32Rect(0, 0, _width, _height);
             bitmap.WritePixels(rect, _pixels, _stride, 0);
             return bitmap;
+        }
+
+        private static void MarkPotentialMinutiaes()
+        {
+            foreach (var minutiaCandidate in _minutiaCandidates)
+            {
+                var x = minutiaCandidate[0];
+                var y = minutiaCandidate[1];
+
+                Debug.WriteLine(x + " " + y);
+
+                _tempIntPixels[x, y] = 2;
+            }
+        }
+
+        //another method...
+        private static float GetCnValue(int x, int y)
+        {
+            //ignore the boundaries cause why not
+            if (x == 0 || y == 0 || x == _width - 1 || y == _height - 1)
+            {
+                return -1f;
+            }
+
+            int[] P =
+            {
+                _intPixels[x + 1, y],           //P1 = P9
+                _intPixels[x + 1, y - 1],       //P2
+                _intPixels[x, y + 1],           //P3
+                _intPixels[x - 1, y + 1],       //P4
+                _intPixels[x - 1, y],           //P5
+                _intPixels[x - 1, y - 1],       //P6
+                _intPixels[x, y - 1],           //P7
+                _intPixels[x + 1, y - 1]        //P8
+            };
+
+            int cnValue = 0;
+
+            for (int i = 0; i < P.Length - 2; i++)
+            {
+                //P1..P8
+                cnValue += Math.Abs(P[i] - P[i + 1]);
+            }
+
+            //P8..P9
+            cnValue += Math.Abs(P[7] - P[0]);
+
+            cnValue = cnValue / 2;
+
+            return cnValue;
+
         }
 
         private static void InitIntImage()
@@ -147,21 +205,21 @@ namespace Biometrics.Classes
                 {
                     int j = PositionInArray(x, y);
 
-                    if (_intPixels[x, y] == 0)
+                    if (_tempIntPixels[x, y] == 0)
                     {
                         _pixels[j + 2] = 255;
                         _pixels[j + 1] = 255;
                         _pixels[j] = 255;
                     }
 
-                    if (_intPixels[x, y] == 1)
+                    if (_tempIntPixels[x, y] == 1)
                     {
                         _pixels[j + 2] = 0;
                         _pixels[j + 1] = 0;
                         _pixels[j] = 0;
                     }
 
-                    if (_intPixels[x, y] == 2)
+                    if (_tempIntPixels[x, y] == 2)
                     {
                         _pixels[j + 2] = 255;
                         _pixels[j + 1] = 0;
